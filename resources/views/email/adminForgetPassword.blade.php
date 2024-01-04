6992cc43c2c914d1e141a6951de73cdba7e763eb
Rose-Evans11 <roseevangelyn.bellen11.reb@gmail.com>
2024-01-04 21:19:08 +0800

    Change email link for auth

--- resources/views/email/adminForgetPassword.blade.php
+++ resources/views/email/adminForgetPassword.blade.php
@@ -1,10 +1,11 @@
 <h4>Forget Password Email</h4>
 
 Greetings!
-
-   
+<br/>
+<br/>
 You can now reset your password from below link:
+<br/>
 <a href="{{ route('admin.reset.password.get', $token) }}">Reset Password</a>
-
+<br/>
 Best regards,
 FIRMS

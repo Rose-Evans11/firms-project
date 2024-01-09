cdfeb9bc280d9df922c7a5ac590b724070792c10
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-01-09 20:03:08 +0800

    change password

--- app/Http/Controllers/farmerController.php
+++ app/Http/Controllers/farmerController.php
@@ -42,7 +42,7 @@ class farmerController extends Controller
             'validIDNumber'=> 'required',Rule::unique('users', 'validIDNumber'),
         ]);
         
-        //$incomingFields['password'] = bcrypt($incomingFields['password']);
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
         //$imagePhoto = $request->file('photo')->store('public/storage');
         //$imageValidIDPhoto = $request->file('validIDPhoto')->store('public/storage');
         if ($request->hasFile('photo')) {
@@ -61,7 +61,7 @@ class farmerController extends Controller
             'birthdate' => $request->get('birthdate'),
             'age' => $request->get('age'),
             'email' => $request->get('email'),
-            'password' => $request->get('password')->bcrypt(),
+            'password' => $request->get('password'),
             'barangayAddress' => $request->get('barangayAddress'),
             'contactNumber' => $request->get('contactNumber'),
             'validIDNumber' => $request->get('validIDNumber'),

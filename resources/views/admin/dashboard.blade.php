0714d96cfd5e7bc86bd2820ff3a7941c934e6582
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-01-09 20:40:22 +0800

    change route for admin pending

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-2">
             <div class="card text-white mb-3" style="background-color:#6CA26D; max-height: 125px">
                 <div class="card-body">
-                  <p class="card-title text-center fw-bolder"> <a href="{{route('insurance.pending')}}" style="text-decoration: none; color:white"> Pending Report</a> </p>
+                  <p class="card-title text-center fw-bolder"> <a href="{{route('admin.pending')}}" style="text-decoration: none; color:white"> Pending Report</a> </p>
                   <p class="card-text fs-1 text-center fw-bolder">{{DB::table('insurances')->where('status', 'Pending')->get()->count()}}</p>
                 </div>
             </div>

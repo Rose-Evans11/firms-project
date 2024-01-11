be13b2cbe3f196f4587c73bebbd990373fc41126
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-01-11 23:04:12 +0800

    add search rice insurance

--- app/Http/Controllers/adminInsuranceController.php
+++ app/Http/Controllers/adminInsuranceController.php
@@ -370,4 +370,29 @@ class adminInsuranceController extends Controller
           'query'=>'min:2'
        ]);
     }
+    public function admin_insurance_rice_find(Request $request, insurance $insurance){ //to search and find
+
+        if (Auth::guard('admin')->check())
+        {
+            $search_text = $request->input('query');
+            $insurance = insurance::where(function($query) use ($search_text) {
+                $query->where('farmersID', 'like', '%' . $search_text . '%')
+                      ->orWhere('firstName', 'like', '%' . $search_text . '%')
+                      ->orWhere('lastName', 'like', '%' . $search_text . '%')
+                      ->orWhere('cropName', 'like', '%' . $search_text . '%')
+                      ->orWhere('insuranceType', 'like', '%' . $search_text . '%')
+                      ->orWhere('rsbsa', 'like', '%' . $search_text . '%')
+                      ->orWhere('cicNumber', 'like', '%' . $search_text . '%')
+                      ->orWhere('cocNumber', 'like', '%' . $search_text . '%')
+                      ->orWhere('created_at', 'like', '%' . $search_text . '%');
+            })->whereIn('insuranceType', 'Rice')->paginate(10);
+                  
+        return view('admin/search_insurance_rice_view',['insurances'=>$insurance]);
+
+        }
+         return redirect('firms/admin/login')->withInput()->with('errmessage', 'Please Login First!');
+        $request->validate([
+          'query'=>'min:2'
+       ]);
+    }
 }

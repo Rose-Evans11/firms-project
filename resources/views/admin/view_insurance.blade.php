7838b4c79678cc014a6f3b9fd52ce42ee3be6bc2
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-01-12 01:21:14 +0800

    remove send request letter

--- resources/views/admin/view_insurance.blade.php
+++ resources/views/admin/view_insurance.blade.php
@@ -658,17 +658,12 @@
                 <div class="col-md-12"> 
                   <div class="form-group">
                     <label for="requestLetter"  class="col-lg-12 control-label">Request Letter:</label>
-                    <textarea id="requestLetter" name="requestLetter" class=" form-control col-lg-12" required value="{{$insurances->requestLetter}}"> </textarea>
+                    <textarea id="requestLetter" name="requestLetter" class=" form-control col-lg-12" required value="{{$insurances->requestLetter}}" @readonly(true)> </textarea>
                   </div>
                 </div>
               </div>
             </div>
             <div class="row justify-content-end mt-2">
-              <div class="col-md-6">
-                <div class="col-lg-12 d-flex justify-content-end">
-                 <button style="text-decoration: none; color:white" class="btn btn-success" style="width: 100%" type="submit"> Send Request Letter</button>
-                </div>
-              </div>
                 <div class="col-md-6">
                   <div class="col-lg-12 d-flex justify-content-end">
                    <a href="{{ URL::previous() }}" style="text-decoration: none; color:white" class="btn btn-success" style="width: 100%"> Back</a></Button>

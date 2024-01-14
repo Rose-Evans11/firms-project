512b7128fcff31fa2be3f421d81fb400f1907aeb
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-01-15 06:17:19 +0800

    change src

--- resources/views/farmer/profile.blade.php
+++ resources/views/farmer/profile.blade.php
@@ -448,7 +448,7 @@
                     -->
                       <div class="row">
                        <!-- <img src="data:image/png;base64, base64_decode()" alt=" Profile Photo" /> -->
-                        <img id="img_id" alt="Your Image" style="width: 150px; height:auto" class="img-fluid" src="{{ URL::asset('public/profile_image_location/' . Auth::User()->photo) }}">
+                        <img id="img_id" alt="Your Image" style="width: 150px; height:auto" class="img-fluid" src="{{Auth::User()->photo }}">
                         </div>
                       </div>
                     </div>

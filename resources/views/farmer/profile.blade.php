ee20250cc000588f2692f23152c1b5a3f49b1dbf
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-02-02 12:35:31 +0800

    edit src

--- resources/views/farmer/profile.blade.php
+++ resources/views/farmer/profile.blade.php
@@ -104,7 +104,7 @@
                     -->
                       <div class="row">
                         <div class="form-group my-5 text-center">
-                       <img id="img_id" alt="Your Image" style="width: 150px; height:auto" class="img-fluid" src>
+                       <img id="img_id" alt="Your Image" style="width: 150px; height:auto" class="img-fluid" src="{{ asset('public/profile_image_location/'.$user->photo) }}">
                         </div>
                       </div>
                     </div>

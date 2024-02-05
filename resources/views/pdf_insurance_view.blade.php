d35a8e26093c4fb33b6baf96e8098af2b8924b10
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-02-05 19:56:39 +0800

    add count

--- resources/views/pdf_insurance_view.blade.php
+++ resources/views/pdf_insurance_view.blade.php
@@ -4,9 +4,10 @@
         <div class="row">
             <h4> Insurance Report </h4>
               <div class="col-md-12">
-                  <div class="table-wrapper" style=" width:100%;">
+                  <div class="table-wrapper" style=" width:100%; overflow-x:scroll">
                     <table class="table table-bordered table-striped">
                       <thead>
+                          <th>Insurance ID</th>
                           <th>Crops</th>
                           <th>Insurance Type</th>
                           <th>Farmer's ID</th>
@@ -21,20 +22,27 @@
                           <th>Status Note</th>
                       </thead>
                       <tbody>
-                          <tr>
-                           <td>{{$insurance->cropName}}</td>
-                           <td>{{$insurance->insuranceType}}</td>
-                           <td>{{$insurance->farmersID}}</td>
-                           <td>{{$insurance->firstName}}</td>
-                           <td>{{$insurance->lastName}}</td>
-                           <td>{{$insurance->barangayAddress}}</td>
-                           <td>{{$insurance->cityAddress}}</td>
-                           <td>{{$insurance->dateHarvest}}</td>
-                           <td>{{$insurance->barangayFarm}}</td>
-                           <td>{{$insurance->created_at}}</td>
-                           <td>{{$insurance->status}}</td>
-                           <td>{{$insurance->statusNote}}</td>
-                          </tr>
+                        @if(count($insurances) > 0)
+                        @foreach ($insurances as $insurance)
+                        <tr>
+                         <td>{{$insurance->id}} </td>
+                         <td>{{$insurance->cropName}}</td>
+                         <td>{{$insurance->insuranceType}}</td>
+                         <td>{{$insurance->farmersID}}</td>
+                         <td>{{$insurance->firstName}}</td>
+                         <td>{{$insurance->lastName}}</td>
+                         <td>{{$insurance->barangayAddress}}</td>
+                         <td>{{$insurance->cityAddress}}</td>
+                         <td>{{$insurance->dateHarvest}}</td>
+                         <td>{{$insurance->barangayFarm}}</td>
+                         <td>{{$insurance->created_at}}</td>
+                         <td>{{$insurance->status}}</td>
+                         <td>{{$insurance->statusNote}}</td>
+                        </tr>
+                        @endforeach
+                        @else
+                         <tr><td>No result found!</td></tr>
+                        @endif
                       </tbody>
                     </table>
                 </div>

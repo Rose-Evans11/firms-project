b4fd97ca958d0982728eae7614b92c7a3b6f346b
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-02-09 21:54:32 +0800

    remove fromcollection

--- app/Http/Controllers/generateAdminReportController.php
+++ app/Http/Controllers/generateAdminReportController.php
@@ -153,17 +153,3 @@ class generateAdminReportController extends Controller
     }
 }
 
-class MyExport implements FromCollection
-{
-    private $data;
-
-    public function __construct($data)
-    {
-        $this->data = $data;
-    }
-
-    public function collection()
-    {
-        return collect($this->data);
-    }
-}

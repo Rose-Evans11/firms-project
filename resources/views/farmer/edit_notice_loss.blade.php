3767268f214a6240409b12a7a43a051de6bba809
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-02-12 23:32:46 +0800

    edit update button

--- resources/views/farmer/edit_notice_loss.blade.php
+++ resources/views/farmer/edit_notice_loss.blade.php
@@ -301,7 +301,9 @@
           <br/>
           <div class="row">
             <div class="col-md-6">
-              <button class="btn btn-sm btn-success m-2 col-lg-12" type="submit" style="width:100%">Update</button>
+              <div class="col-lg-12 d-flex justify-content-end">
+                <button class="btn btn-sm btn-success m-2 col-lg-12" type="submit" style="width:100%">Update</button>
+              </div>
             </div>
           </div>
         </div>

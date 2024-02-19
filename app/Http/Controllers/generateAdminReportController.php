a488b3378ea8483a6d9d3f3775342382fedca32a
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-02-19 21:30:10 +0800

    edit font in pdf

--- app/Http/Controllers/generateAdminReportController.php
+++ app/Http/Controllers/generateAdminReportController.php
@@ -484,7 +484,7 @@ class generateAdminReportController extends Controller
  
         $pdf->render();
         $dompdf = $pdf->getDomPDF();
-        $font = $dompdf->getFontMetrics()->get_font("helvetica", "bold");
+        $font = $dompdf->getFontMetrics()->get_font("roboto");
         $dompdf->get_canvas()->page_text(34, 18, "{PAGE_NUM} / {PAGE_COUNT}", $font, 10, array(0, 0, 0));
         
         return $pdf->download('indemnity.pdf');

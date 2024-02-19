a5d33ddde0603ed299f784e8e05ef6ba103eff52
Rose Evangelyn Bellen <roseevangelyn.bellen11.reb@gmail.com>
2024-02-19 21:11:39 +0800

    add link in pdfs

--- resources/views/pdf_user_view.blade.php
+++ resources/views/pdf_user_view.blade.php
@@ -1,5 +1,15 @@
 <html>
   <head>
+     <!-- Scripts -->
+     <script src="{{ asset('js/app.js') }}" defer></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+     <!-- Styles -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+     <!-- Bootstrap Icons CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" rel="stylesheet">
+     <!-- custom CSS should come after Bootstrap CSS -->
+     <link href="{{ asset('css/app.scss') }}" rel="stylesheet">
   </head>
   <body>
       <div class="row d-flex justify-content-center">
